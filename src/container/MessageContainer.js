1ad91fe36ad94dbaadde2259b93464f01e7286ac
liuuu <next@yudeMacBook-Air.local>
2017-12-26 10:30:13 +0800

    make MessageContainer subscribe to `Subscription newChannelMessage`

--- src/container/MessageContainer.js
+++ src/container/MessageContainer.js
@@ -5,27 +5,91 @@ import { Comment } from 'semantic-ui-react';
 
 import Messages from '../components/Messages';
 
-const MessageContainer = ({ data: { loading, messages } }) =>
-  (loading || !messages ? null : (
-    <Messages>
-      <Comment.Group>
-        {messages.map(m => (
-          <Comment key={`${m.id}-message`}>
-            <Comment.Content>
-              <Comment.Author as="a">{m.user.username}</Comment.Author>
-              <Comment.Metadata>
-                <div>{m.created_at}</div>
-              </Comment.Metadata>
-              <Comment.Text>{m.text}</Comment.Text>
-              <Comment.Actions>
-                <Comment.Action>Reply</Comment.Action>
-              </Comment.Actions>
-            </Comment.Content>
-          </Comment>
-        ))}
-      </Comment.Group>
-    </Messages>
-  ));
+/*
+  type Subscription {
+    newChannelMessage(channelId:Int!) :Message!
+  }
+*/
+
+const newChannelMessageSubscription = gql`
+  subscription($channelId: Int!) {
+    newChannelMessage(channelId: $channelId) {
+      id
+      text
+      user {
+        username
+      }
+      created_at
+    }
+  }
+`;
+
+class MessageContainer extends React.Component {
+  componentWillMount() {
+    this.unsubscribe = this.subscribe(this.props.channelId);
+  }
+
+  componentWillReceiveProps({ channelId }) {
+    if (this.props.channelId !== channelId) {
+      if (this.unsubscribe) {
+        this.unsubscribe();
+      }
+      this.unsubscribe = this.subscribe(channelId);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
+  }
+
+  subscribe = channelId =>
+    this.props.data.subscribeToMore({
+      document: newChannelMessageSubscription,
+      variables: {
+        channelId,
+      },
+      updateQuery: (prev, { subscriptionData }) => {
+        console.log('dfsdf');
+        console.log('prev', prev);
+        console.log('subscriptionData', subscriptionData.data.newChannelMessage);
+
+        if (!subscriptionData) {
+          return prev;
+        }
+
+        return {
+          ...prev,
+          messages: [...prev.messages, subscriptionData.data.newChannelMessage],
+        };
+      },
+    });
+
+  render() {
+    const { data: { loading, messages } } = this.props;
+    return loading ? null : (
+      <Messages>
+        <Comment.Group>
+          {messages.map(m => (
+            <Comment key={`${m.id}-message`}>
+              <Comment.Content>
+                <Comment.Author as="a">{m.user.username}</Comment.Author>
+                <Comment.Metadata>
+                  <div>{m.created_at}</div>
+                </Comment.Metadata>
+                <Comment.Text>{m.text}</Comment.Text>
+                <Comment.Actions>
+                  <Comment.Action>Reply</Comment.Action>
+                </Comment.Actions>
+              </Comment.Content>
+            </Comment>
+          ))}
+        </Comment.Group>
+      </Messages>
+    );
+  }
+}
 
 const messagesQuery = gql`
   query($channelId: Int!) {
@@ -44,4 +108,7 @@ export default graphql(messagesQuery, {
   variables: props => ({
     channelId: props.channelId,
   }),
+  options: {
+    fetchPolicy: 'network-only',
+  },
 })(MessageContainer);
